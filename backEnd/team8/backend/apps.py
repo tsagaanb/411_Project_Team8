07beefa509c7f14736044e5ba74d8bfee168a3d8
Chloe <Clesson295@gmail.com>
2023-11-28 14:29:52 -0500

    changes

--- backEnd/team8/backend/apps.py
+++ backEnd/team8/backend/apps.py
@@ -4,3 +4,13 @@ from django.apps import AppConfig
 class BeckendConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "beckend"
+
+"""another webiste says to do it like this:
+from django.apps import AppConfig
+
+
+class RockNRollConfig(AppConfig):
+    name = "rock_n_roll"
+    verbose_name = "Rock ’n’ roll"
+    
+    """
