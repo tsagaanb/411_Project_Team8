5a867edd4341a4a3403c592a63babd1f78b6155e
Tsagaandari <tsagaandari0604@crc-dot1x-nat-10-239-183-122.bu.edu>
2023-11-30 15:50:14 -0500

    Updating views.py to use user input

--- backEnd/team8/backend/views.py
+++ backEnd/team8/backend/views.py
@@ -1,26 +1,27 @@
-from django.shortcuts import render
 import requests
-
-# Create your views here.
+from django.shortcuts import render
 from django.http import HttpResponse
 
+def generate_recipe(request):
+    if request.method == 'POST':
+        # Assuming you have a form with a field named 'ingredients'
+        ingredients = request.POST.get('ingredients')
 
-def index(request):
-    #return HttpResponse("Hello, world. You're at the polls index.")
-#encapsulates the logic that defines how your application interacts with users' requests.
-#this is the file where you would write calls to the api
-    foods = "apples, user input"
+        # Replace 'API_ENDPOINT' with the actual recipe API endpoint
+        recipe_api_url = f'https://api.spoonacular.com/recipes/findByIngredients?ingredients={ingredients}&number=10&limitLicense=true&ranking=1&ignorePantry=false'
 
-    url = f"https://api.spoonacular.com/recipes/findByIngredients?ingredients={foods}&number=10&limitLicense=true&ranking=1&ignorePantry=false"
+        try:
+            # Make a GET request to the recipe API
+            response = requests.get(recipe_api_url)
 
-   # should have user input 
-    response = requests.get(url)
-    #acessing the spenacular api and getting the response
-
-    if response.status_code == 200:
-        data = response.json()
-        return render(request, 'my_template.html', {'api_data': data})
-    else:
-        error_message = f"Error: {response.status_code}"
-        return render(request, 'error_template.html', {'error_message': error_message})
-
+            if response.status_code == 200:
+                # If the API call is successful, retrieve the recipe data
+                recipe_data = response.json()
+                # Process the recipe data as needed
+                # For example, extract recipe details and render them in a template
+                return render(request, 'recipe_template.html', {'recipe': recipe_data})
+            else:
+                return HttpResponse(f"Failed to fetch recipe. Error: {response.status_code}")
+        
+        except requests.RequestException as e:
+            return HttpResponse(f"Error fetching recipe: {str(e)}")
