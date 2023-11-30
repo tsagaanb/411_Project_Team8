70ada7d2255fc36e26785585927c9ab17fb61e61
Tsagaandari <tsagaandari0604@crc-dot1x-nat-10-239-183-122.bu.edu>
2023-11-30 13:10:33 -0500

    Adding models to admin.py

--- backEnd/team8/backend/admin.py
+++ backEnd/team8/backend/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Recipe, Ingredient  # Importing models 
 
-# Register your models here.
+# Register your models here
+admin.site.register(Recipe)
+admin.site.register(Ingredient)
