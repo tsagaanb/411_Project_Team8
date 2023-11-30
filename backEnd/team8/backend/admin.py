c6434b5c081d895fd6ba8a1279eddd92fd857535
Tsagaandari <tsagaandari0604@crc-dot1x-nat-10-239-183-122.bu.edu>
2023-11-30 13:21:35 -0500

    Adding UserRecipe model

--- backEnd/team8/backend/admin.py
+++ backEnd/team8/backend/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Recipe, Ingredient  # Importing models 
+from .models import UserRecipe, Recipe, Ingredient # Importing models 
 
 # Register your models here
+admin.site.register(UserRecipe)
 admin.site.register(Recipe)
 admin.site.register(Ingredient)
