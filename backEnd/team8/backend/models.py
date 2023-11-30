e2cbd71e286eb0fb6c3fc9d4d3ec21a73dba964f
Evan Akinyemi <evanakinyemi@crc-dot1x-nat-10-239-90-148.bu.edu>
2023-11-30 18:17:22 -0500

    added user

--- backEnd/team8/backend/models.py
+++ backEnd/team8/backend/models.py
@@ -15,12 +15,12 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)"""
 
-class UserRecipe(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
-    saved_at = models.DateTimeField(auto_now_add=True) 
 
-    
+
+class User(models.Model):
+           User_name = models.CharField(max_length = 50)
+           User_email = models.CharField(max_length = 200)
+           User_pass = models.CharField(max_length = 50)
 
 #want to store recipie name, ingredients, and instructions
 class Recipe(models.Model):
@@ -31,6 +31,11 @@ class Ingredient(models.Model):
     ingredient_name = models.CharField(max_length=200)
     #ingredient_amount = models.IntegerField(default=0) #may need one of these for each ingredient
     ingredient_amount = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+
+class UserRecipe(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    saved_at = models.DateTimeField(auto_now_add=True) 
 
     def __str__(self): #string representation of the object, dont know if we need this
         return self.recipe_name
