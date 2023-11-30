1abc36943a9e11f1bd52532c435268515fd12fe9
Chloe <Clesson295@gmail.com>
2023-11-30 18:09:49 -0500

    such and such

--- backEnd/team8/backend/urls.py
+++ backEnd/team8/backend/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from django.urls import path, include
+from django.contrib.auth.views import LogoutView
+
 
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('accounts/', include('allauth.urls')),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
