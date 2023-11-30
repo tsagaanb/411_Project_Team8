269b3fe706300dc11f61fbe435b2bcdfb8932438
Chloe <Clesson295@gmail.com>
2023-11-30 14:14:00 -0500

    api save to database

--- backEnd/team8/backend/utils.py
+++ backEnd/team8/backend/utils.py
@@ -1,5 +1,13 @@
-from .models import MyModel
+from .models import UserRecipe, Recipe, Ingredient
 import requests
 
 #file that acceses api
 
+def save_api_data_to_model(api_data):
+    for item in api_data:
+        Recipe.objects.create(
+            field1=item['field1'],
+            field2=item['field2'],
+            # Add more fields as needed
+        )
+        
