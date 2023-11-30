7079d979ae697e7a02fa11b909687a4949df1f72
Chloe <Clesson295@gmail.com>
2023-11-30 13:43:21 -0500

    branch api?

--- backEnd/team8/backend/views.py
+++ backEnd/team8/backend/views.py
@@ -4,6 +4,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+from django.shortcuts import render
+import requests
+
+# Create your views here.
+from django.http import HttpResponse
+
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    #return HttpResponse("Hello, world. You're at the polls index.")
 #encapsulates the logic that defines how your application interacts with users' requests.
+#this is the file where you would write calls to the api
+
+    url = 'https://api.spoonacular.com/recipes/findByIngredients?ingredients=carrots,tomatoes&number=10&limitLicense=true&ranking=1&ignorePantry=false'
+    response = requests.get(url)
+    #acessing the spenacular api and getting the response
+
+    if response.status_code == 200:
+        data = response.json()
+        return render(request, 'my_template.html', {'api_data': data})
+    else:
+        error_message = f"Error: {response.status_code}"
+        return render(request, 'error_template.html', {'error_message': error_message})
+
