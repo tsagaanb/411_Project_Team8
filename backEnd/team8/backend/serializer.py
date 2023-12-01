872c68d743642c90ba46f72081d4fd44f0d4cbc0
Chloe <Clesson295@gmail.com>
2023-12-01 13:34:51 -0500

    new file

---
+++ backEnd/team8/backend/serializer.py
@@ -0,0 +1,15 @@
+
+#Serializers are basically used to convert complex data to native Python datatypes that can then be easily rendered 
+#into JSON(Which we are going to use in React#
+
+#for connecting front to backend
+
+from rest_framework import serializers 
+from . models import *
+  
+class ReactSerializer(serializers.ModelSerializer): 
+    class Meta: 
+        model = React 
+        fields = ['name', 'detail']
+
+        
