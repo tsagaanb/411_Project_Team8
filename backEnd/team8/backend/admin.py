2c78f1aaa8cf1d528110757d54efd66d71448bd5
Tsagaandari <tsagaandari0604@crc-dot1x-nat-10-239-183-122.bu.edu>
2023-11-30 19:12:42 -0500

    Fixed errors

--- backEnd/team8/backend/admin.py
+++ backEnd/team8/backend/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import UserRecipe, Recipe, Ingredient # Importing models 
+from .models import User, UserRecipe, Recipe, Ingredient, Rating # Importing models 
 
 # Register your models here
 admin.site.register(User)
