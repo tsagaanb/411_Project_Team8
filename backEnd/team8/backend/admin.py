348dd1dbf0205d3db67f9184692326c2dc00d6f0
Tsagaandari <tsagaandari0604@crc-dot1x-nat-10-239-183-122.bu.edu>
2023-11-30 19:07:48 -0500

    Adding User and Rating into admin.py

--- backEnd/team8/backend/admin.py
+++ backEnd/team8/backend/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from .models import UserRecipe, Recipe, Ingredient # Importing models 
 
 # Register your models here
+admin.site.register(User)
 admin.site.register(UserRecipe)
 admin.site.register(Recipe)
 admin.site.register(Ingredient)
+admin.site.register(Rating)
+
