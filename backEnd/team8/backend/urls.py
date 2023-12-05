91f1307b1b0648f87007773c37476ba341bd6598
Chloe <Clesson295@gmail.com>
2023-12-05 18:26:26 -0500

    2nd api support

--- backEnd/team8/backend/urls.py
+++ backEnd/team8/backend/urls.py
@@ -5,12 +5,14 @@ from django.contrib import admin
 #from django.conf.urls import url 
 #from core.views import * #not sure
 from . import views
+from .views import get_nutrients #second api
 
 urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('get_recipes/', views.get_recipes, name='get_recipes'),  # without parameter
     path('get_recipes/<str:ingredients>/', views.get_recipes, name='get_recipes_with_ingredients'),  # with parameter
+    path('', get_nutrients, name='get_nutrients'), #for second api
 ]
 
 #could also be:
