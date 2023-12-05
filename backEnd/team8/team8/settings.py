45107f458f2d644a7ac61fb1c11760ca9e4e9267
Evan Akinyemi <evanakinyemi@crc-dot1x-nat-10-239-45-225.bu.edu>
2023-12-05 13:46:47 -0500

    changed settings

--- backEnd/team8/team8/settings.py
+++ backEnd/team8/team8/settings.py
@@ -39,13 +39,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'allauth',
-    'allauth.account',
-    'allauth.socialaccount',
-    'allauth.socialaccount.providers.google',
-    'rest_framework',
-    'core',
-    'corsheaders',
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
+    "allauth.socialaccount.providers.google",
+    "rest_framework",
+    "core",
+    "corsheaders",
 
 ]
 #step 7 of connecting front to backend 
@@ -94,23 +94,23 @@ DATABASES = {
 }
 
 AUTHENTICATION_BACKENDS = [
-    'django.contrib.auth.backends.ModelBackend',
-    'allauth.account.auth_backends.AuthenticationBackend',
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
 ]
 
 SOCIALACCOUNT_PROVIDERS = {
-    'google': {
-        'SCOPE': [
-            'profile',
-            'email',
+    "google": {
+        "SCOPE": [
+            "profile",
+            "email",
         ],
-        'AUTH_PARAMS': {
-            'access_type': 'online',
+        "AUTH_PARAMS": {
+            "access_type": "online",
         },
-        'APP': {
-            'client_id': '<YOUR_CLIENT_ID>', ######
-            'secret': '<YOUR_SECRET>', ########
-            'key': ''
+        "APP": {
+            "client_id": "<YOUR_CLIENT_ID>", ######
+            "secret": "<YOUR_SECRET>", ########
+            "key": ""
         }
     }
 }
@@ -157,5 +157,5 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
