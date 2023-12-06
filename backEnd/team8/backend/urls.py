2365133153b810576dd7a58ef151bd9af83cf6b1
Tsagaandari <tsagaandari0604@Tsagaandaris-MacBook-Pro.local>
2023-12-05 21:10:48 -0500

    Edited a bug in urls.py

--- backEnd/team8/backend/urls.py
+++ backEnd/team8/backend/urls.py
@@ -15,7 +15,6 @@ urlpatterns = [
     path('get_recipes/', views.get_recipes, name='get_recipes'),  # without parameter
     path('get_recipes/<str:ingredients>/', views.get_recipes, name='get_recipes_with_ingredients'),  # with parameter
     path('get_news/', get_news, name='get_news'),
-    #path('rate_product/<int:recipe_id>/', rate_product, name='rate_product'), rating system
-    path('submit_rating/<int:recipe_id>/', views.rate_recipe, name='rate_recipe'),
+    path('submit_rating/<int:recipe_id>/', views.submit_rating, name='submit_rating'),
 
 ]
