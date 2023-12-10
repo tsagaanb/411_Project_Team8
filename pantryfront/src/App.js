2b5e70683398145913c67ed72d2c748da714c62d
Tsagaandari <tsagaandari0604@crc-dot1x-nat-10-239-7-169.bu.edu>
2023-12-10 18:05:51 -0500

    homepage navigation attempt

--- pantryfront/src/App.js
+++ pantryfront/src/App.js
@@ -1,14 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import LoginButton from "./login"; 
 import { gapi } from "gapi-script";
 import logo from "./components/Images/smallerLogo.png"
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Homepage from './homepage'; 
 
 const clientId = "312871000003-3d2pb1d7jlktfuhvkcl1lpkal499s7l7.apps.googleusercontent.com";
 
 export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
   useEffect(() => {
     function start() {
       gapi.client.init({
@@ -21,14 +22,15 @@ export default function App() {
 
       // Handle the initial sign-in state
       updateSignInStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-      
     }
     
 
     function updateSignInStatus(isSignedIn) {
       if (isSignedIn) {
+        setIsAuthenticated(isSignedIn);
         // User is signed in, update your UI or perform actions accordingly
         console.log("User is signed in");
+
       } else {
         // User is signed out, update your UI or perform actions accordingly
         console.log("User is signed out");
@@ -36,6 +38,9 @@ export default function App() {
     }
 
     gapi.load("client:auth2", start);
+
+
+    
   }, []);
 
   return (
@@ -54,7 +59,7 @@ export default function App() {
       </b>
       </p>
       <p><b>
-      users with updates news related to the food world all over the globe using NEWS API.
+      users with updated news related to the food world all over the globe using NEWS API.
       </b>
       </p>
       <p>
@@ -69,12 +74,14 @@ export default function App() {
       </div>
       <Routes>
           <Route exact path="/" component={LoginButton} />
-          <Route exact path="/homepage" element={<Homepage />} /> 
+          {isAuthenticated ? (
+            <Route path="/homepage" element={<Homepage />} />
+          ) : (
+            <Route path="*" element={null} />
+          )}
           {/* not sure how this is supposed to work, the home page keeps appearing
               on the bottom even if the user is not logged in.
               But when i delete the line above, it says homepage cannot be found.*/}
-           
-
         </Routes>
     </div>
     </Router>
