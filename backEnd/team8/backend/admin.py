a7eb2bd9954741c1ee69165d539a8178fdc3fb21
Tsagaandari <tsagaanb@bu.edu>
2023-12-11 16:15:42 -0500

    Deleted Rating, which doesn't exist anymore

--- backEnd/team8/backend/admin.py
+++ backEnd/team8/backend/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
-from .models import User, UserRecipe, Recipe, Ingredient, Rating # Importing models 
+from .models import User, UserRecipe, Recipe, Ingredient # Importing models 
 
 # Register your models here
 admin.site.register(User)
 admin.site.register(UserRecipe)
 admin.site.register(Recipe)
 admin.site.register(Ingredient)
-# admin.site.register(Rating)
 
