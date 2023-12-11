9a5bec15a914e10b4ad922f745b6b8c91277ed86
Tsagaandari <tsagaanb@bu.edu>
2023-12-10 22:00:07 -0500

    Google auth issues

--- pantryfront/src/login.js
+++ pantryfront/src/login.js
@@ -1,21 +1,21 @@
-import { GoogleLogin } from 'react-google-login';
+import { GoogleLogin } from 'react-oauth/google';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 //import Homepage from './homepage.js'; 
 
-const clientId = "312871000003-3d2pb1d7jlktfuhvkcl1lpkal499s7l7.apps.googleusercontent.com";
+//const clientId = "312871000003-3d2pb1d7jlktfuhvkcl1lpkal499s7l7.apps.googleusercontent.com";
+<GoogleOAuthProvider clientId="http://312871000003-3d2pb1d7jlktfuhvkcl1lpkal499s7l7.apps.googleusercontent.com">...</GoogleOAuthProvider>;
 
 function Login(){
     const navigate = useNavigate();
-    const onSuccess = (res) => {
-        console.log("LOGIN SUCCESS! Current user: ", res.profileObj);
-        // redirect to the home page after successful login
-        navigate('/homepage');
-    }
-    const onFailure = (res) => {
-        console.log("LOGIN FAILED! res: ", res);
-    }
-
+    <GoogleLogin
+  onSuccess={credentialResponse => {
+    console.log(credentialResponse);
+  }}
+  onError={() => {
+    console.log('Login Failed');
+  }}
+/>;
     return(
     <div id="signInButton">
         <GoogleLogin 
